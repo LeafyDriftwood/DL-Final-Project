3a74b8bbdc847e8950b72b9529bbde806443a524
Manav Chakravarthy <77932039+LeafyDriftwood@users.noreply.github.com>
2021-12-03 21:48:06 -0500

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
+from tensorflow.data import Dataset
 from tensorflow.keras.utils import Sequence
 import numpy as np
+import keras
+
 
 from utils import get_timestamp
 
@@ -14,26 +17,34 @@ class SuicidalDataset(Sequence):
         self.current = current
         self.timestamp = timestamp
         self.random = random
+        self.batch_size = 100
 
     def __len__(self):
         return len(self.label)
 
     # given an item, returns a list of labels, tweet features, temporal tweet features, and the timestamp
     def __getitem__(self, item):
-        labels = tf.Tensor(self.label[item])
+        labels = (self.label[item])
         tweet_features = self.tweet[item]
+
         if self.current:
             result = self.temporal[item]
             if self.random:
                 np.random.shuffle(result)
-            temporal_tweet_features = tf.Tensor(result)
-            timestamp = tf.Tensor(get_timestamp(self.timestamp[item]))
+            
+            temporal_tweet_features = tf.constant(result)
+            timestamp = tf.constant(get_timestamp(self.timestamp[item]))
+            
         else:
             if len(self.temporal[item]) == 1:
                 temporal_tweet_features = np.zeros((1, 768), dtype=tf.float32)
                 timestamp = np.zeros((1, 1), dtype=tf.float32)
-            else:
-                temporal_tweet_features = tf.Tensor(self.temporal[item][1:])
-                timestamp = tf.Tensor(get_timestamp(self.timestamp[item][1:]))
+                
 
+            else:
+                temporal_tweet_features = tf.constant(self.temporal[item][1:])
+                timestamp = tf.constant(get_timestamp(self.timestamp[item][1:]))
+
+        
         return [labels, tweet_features, temporal_tweet_features, timestamp]
+
